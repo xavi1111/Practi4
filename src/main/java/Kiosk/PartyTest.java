a9c61e94c13915bc10be38efebaab408527e3955
viloc <viseap98Serrano>
2018-12-28 17:42:12 +0100

    Creada classe test de la classe party i test del constructor de party

---
+++ src/main/java/Kiosk/PartyTest.java
@@ -0,0 +1,35 @@
+package Kiosk;
+
+import Exceptions.PartyException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PartyTest {
+    static Party party;
+    @BeforeAll
+    static void setParty(){
+        try {
+            party = new Party("PP");
+        } catch (PartyException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void PartyConstructorTest () {
+        assertThrows(PartyException.class,()->{ new Party(null);});
+    }
+
+    @org.junit.jupiter.api.Test
+    void equalsTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void hashCodeTest() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void toStringTest() {
+    }
+}
