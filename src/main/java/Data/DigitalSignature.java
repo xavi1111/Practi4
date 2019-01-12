d81a5643bea4af69ccaa280b5928574122226798
Xavi <xavierllubes11@gmail.com>
2019-01-12 18:50:16 +0100

    Afegits mètodes classe DigitalSignature.

--- src/main/java/Data/DigitalSignature.java
+++ src/main/java/Data/DigitalSignature.java
@@ -1,4 +1,33 @@
 package Data;
 
+import Exceptions.DigitalSignatureException;
+
 public class DigitalSignature {
+    private final String firma;
+
+    public DigitalSignature (String address) throws DigitalSignatureException {
+        if (!(address == null))
+            this.firma = address;
+        else
+            throw new DigitalSignatureException("Null signature");
+    }
+
+    public String getSignature() { return firma; }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DigitalSignature firma1 = (DigitalSignature) o;
+        return firma.equals(firma1.firma);
+    }
+
+    @Override
+    public int hashCode() {
+        return firma.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Signatura{" + "='" + firma + '\'' + '}';
+    }
 }
---
+++ src/main/java/Exceptions/DigitalSignatureException.java
@@ -0,0 +1,7 @@
+package Exceptions;
+
+public class DigitalSignatureException extends Exception {
+    public DigitalSignatureException(String msg) {
+        super(msg);
+    }
+}
