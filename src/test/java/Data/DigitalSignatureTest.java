f7c17cc3acb7478b714fc3c8910ea3de26f612cb
viloc <viseap98serrano>
2019-01-12 18:55:54 +0100

    Afegit mètode test excepcions de DigitalSignature.

--- src/test/java/Data/DigitalSignatureTest.java
+++ src/test/java/Data/DigitalSignatureTest.java
@@ -1,7 +1,23 @@
 package Data;
 
+import Exceptions.DigitalSignatureException;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DigitalSignatureTest {
+    static DigitalSignature firmaTest;
+    @BeforeAll
+    static void setParty(){
+        try {
+            firmaTest = new DigitalSignature("viloc");
+        } catch (DigitalSignatureException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void MailConstructorTest () {
+        assertThrows(DigitalSignatureException.class,()->{ new DigitalSignature(null);});
+    }
 
 }
